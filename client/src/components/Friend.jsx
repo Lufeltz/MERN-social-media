cec279b93e6fa37192fd93899af9dffcb205d742
Lufeltz <lufelt@proton.me>
2023-10-01 23:42:59 -0300

    feat: Implementar a função patchFriend

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -4,7 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "../state";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
+import { useNavigate } from "react-router-dom";
 
-const Friend = ({ friendId, name, subtitle, userPicturePath }) => {};
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  const isFriend = friends.find((friend) => friend._id === friendId);
+};
 
 export default Friend;
