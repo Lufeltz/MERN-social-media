22afdde08a41fb1a6d32ffdb0c787dac44e73cf6
Lufeltz <lufelt@proton.me>
2023-10-01 23:29:28 -0300

    feat: Adicionar componente Friend com as importações iniciais

---
+++ client/src/components/Friend.jsx
@@ -0,0 +1,10 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "../state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {};
+
+export default Friend;
