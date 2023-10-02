638092840c7272a15194f099ac7cf753d7189376
Lufeltz <lufelt@proton.me>
2023-10-02 00:50:20 -0300

    feat: Adicionar PostWidget

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,32 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Friend from "../../components/Friend";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "../../state";
+
+const PostWidget = ({
+  key,
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+
+}
+
+
+export default PostWidget;
