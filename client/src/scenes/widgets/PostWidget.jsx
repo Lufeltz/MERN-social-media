3eefced7de5e6c6c26247f7cf3b46b4a25c297cf
Lufeltz <lufelt@proton.me>
2023-10-03 13:34:13 -0300

    feat: Mapear todos os comentários do post

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -91,8 +91,25 @@ const PostWidget = ({
           </FlexBetween>
         </FlexBetween>
 
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
       </FlexBetween>
 
+      {/* SHOW COMMENTS */}
+      {isComments && (
+        <Box mt="0.5rem">
+          {comments.map((comment, i) => (
+            <Box key={`${name}-${i}`}>
+              <Divider />
+              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                {comment}
+              </Typography>
+            </Box>
+          ))}
+          <Divider />
+        </Box>
+      )}
     </WidgetWrapper>
   );
 };
