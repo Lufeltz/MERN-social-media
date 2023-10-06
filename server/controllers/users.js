40986d683fe7762ae9d145570ae26c74c6284ba4
Lufeltz <lufelt@proton.me>
2023-10-05 23:51:59 -0300

    fix: Corrigir o nome da variável para adicionar ou remover amigos

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,13 +35,13 @@ export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
-    const friend = await User.findById(friend);
+    const friend = await User.findById(friendId);
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
     } else {
-      user.friends.push(friend);
+      user.friends.push(friendId);
       friend.friends.push(id);
     }
     await user.save();
@@ -57,7 +57,7 @@ export const addRemoveFriend = async (req, res) => {
     );
 
     res.status(200).json(formattedFriends);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
 };
