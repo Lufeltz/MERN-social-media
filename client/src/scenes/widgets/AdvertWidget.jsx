d6695cb88656c2c9a7055cd716b12d01079edbf9
Lufeltz <lufelt@proton.me>
2023-10-06 23:51:22 -0300

    feat: Adicionar descrição e link do anunciante

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,6 +24,14 @@ return (
       src="http://localhost:3001/assets/info4.jpeg"
       style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
     />
+    <FlexBetween>
+      <Typography color={main}>MikaCosmetics</Typography>
+      <Typography color={medium}>mikacosmetics.com</Typography>
+    </FlexBetween>
+    <Typography color={medium} m="0.5rem 0">
+      Your pathway to stunning and immaculate beauty and made sure your skin is
+      exfoliating skin and shining like light.
+    </Typography>
   </WidgetWrapper>
 );
 
