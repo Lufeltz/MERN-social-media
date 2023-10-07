6aba49e4e09ec3cd37423612da1d0c948d67255a
Lufeltz <lufelt@proton.me>
2023-10-06 23:34:53 -0300

    feat: Adicionar widget AdvertWidget com importações iniciais

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,7 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
+
+const AdvertWidget = () => {};
+
+export default AdvertWidget;
