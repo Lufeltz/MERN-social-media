e527886859d7521dff934be0fe0055ed785b3e0f
Lufeltz <lufelt@proton.me>
2023-10-08 22:32:51 -0300

    feat: Adicionar retorno da FriendList de cada usuário

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -25,6 +25,30 @@ const FriendListWidget = ({ userId }) => {
   useEffect(() => {
     getFriends();
   }, []);
+
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend list
+      </Typography>
+      <Box display="flex" flexDirection="column" gap="1.5rem">
+        {friends.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.userPicturePath}
+          />
+        ))}
+      </Box>
+    </WidgetWrapper>
+  );
 };
 
 export default FriendListWidget;
