659a4ec84f8be42d511daf1ad832022f488aeda8
Lufeltz <lufelt@proton.me>
2023-10-10 14:06:43 -0300

    chore: Corrigir caminhos das importações e variáveis importadas

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,8 +1,8 @@
-import { Box, useMediaQuey } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Navbar from "scenes/navbar";
+import Navbar from "../../scenes/navbar";
 import FriendListWidget from "../../scenes/widgets/FriendListWidget";
 import MyPostWidget from "../../scenes/widgets/MyPostWidget";
 import PostsWidget from "../../scenes/widgets/PostsWidget";
