8bc16e3ca464ceb5e0b0ed50cdfe5f4a5ea0f693
Lufeltz <lufelt@proton.me>
2023-10-09 23:46:04 -0300

    feat: Adicionar importações e criação de variáveis necessárias

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,5 +1,20 @@
+import { Box, useMediaQuey } from "@mui/material";
+import { useEffect, useState } from "react";
+import { UseSelector, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "../../scenes/widgets/FriendListWidget";
+import MyPostWidget from "../../scenes/widgets/MyPostWidget";
+import PostsWidget from "../../scenes/widgets/PostsWidget";
+import UserWidget from "../../scenes/widgets/UserWidget";
+
 const ProfilePage = () => {
-    return <div>profilepage</div>;
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuey("(min-width: 1000px)");
+
+  return <div>profilepage</div>;
 };
 
 export default ProfilePage;
